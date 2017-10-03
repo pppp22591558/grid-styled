737019245f37b1225f9c6c8c34029f48ad4b85f5
pppp22591558 <pppp29654213@gmail.com>
2017-10-03 16:47:50 +0800

    add dist

---
+++ dist/propTypes.js
@@ -0,0 +1,36 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var responsivePropType = _propTypes2.default.oneOfType([number, string, array]);
+
+var propTypes = {
+  width: responsivePropType,
+  fontSize: responsivePropType,
+  color: responsivePropType,
+  bg: responsivePropType,
+  m: responsivePropType,
+  mt: responsivePropType,
+  mr: responsivePropType,
+  mb: responsivePropType,
+  ml: responsivePropType,
+  mx: responsivePropType,
+  my: responsivePropType,
+  p: responsivePropType,
+  pt: responsivePropType,
+  pr: responsivePropType,
+  pb: responsivePropType,
+  pl: responsivePropType,
+  px: responsivePropType,
+  py: responsivePropType
+};
+
+exports.default = propTypes;
